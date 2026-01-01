bc815d7962348def39a4fd3f78df682d2cb96714
Dwayne Carson <dcarson397@gmail.com>
2026-01-01 17:56:36 -0500

    Add front.png as main screen image with overlay buttons

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,11 @@
 import Link from 'next/link'
 
-const smallPngBase64 = "iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAAKklEQVR4nO3BMQEAAADCoPVPbQhPoAAAAAAAAAAAAAAAAAAAAAAAAAAAADwG7AAAX9E0sQAAAABJRU5ErkJggg==" // small placeholder
-
 export default function Home(){
   return (
-    <main className="center">
-      <div className="container">
-        <header>
-          <h1>World Fantastic</h1>
-        </header>
-
-        <img src={`data:image/png;base64,${smallPngBase64}`} alt="World Fantastic" width="240" height="240" />
-
-        <div className="actions">
+    <main className="home-screen">
+      <div className="home-image-container">
+        <img src="/front.png" alt="World Fantastic" className="home-image" />
+        <div className="home-actions">
           <Link href="/register"><button className="primary">Create New Account</button></Link>
           <Link href="/login"><button className="secondary">Login to Existing Account</button></Link>
         </div>
